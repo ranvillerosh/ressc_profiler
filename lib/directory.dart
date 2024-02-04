4ccfd634f595dfd362eec79d02bf881f7be4de6a
ranvillerosh <76862237+ranvillerosh@users.noreply.github.com>
2024-02-04 15:32:25 +0800

    chooser, main, directory, trainee, trainee_profile

--- lib/directory.dart
+++ lib/directory.dart
@@ -1,6 +1,5 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-import 'package:ressc_profiler/trainee_profile.dart';
 import 'package:transparent_image/transparent_image.dart';
 import 'Data/trainee.dart';
 import 'Data/training.dart';
@@ -24,7 +23,6 @@ class _RESSCDirectory extends State<RESSCDirectory> with TickerProviderStateMixi
   
   @override
   Widget build(BuildContext context) {
-
     List<Trainee> traineeList = [];
     var sampleTraineeData = Trainee("nameFirst","nameMiddle", "nameLast", DateTime.now(), "contactNumber1", "contactNumber2", "emailPersonal", "emailOfficial", "https://firebasestorage.googleapis.com/v0/b/doh-chd-car-portal-app.appspot.com/o/Placeholders%2FprofilePicturePlaceHolder.jpg?alt=media&token=7344d9d2-156d-4fde-88eb-d1253f8b14e2", Training());
     traineeList.add(sampleTraineeData);
--- lib/trainee_profile.dart
+++ lib/trainee_profile.dart
@@ -3,56 +3,46 @@ import 'package:flutter/material.dart';
 import 'package:ressc_profiler/Data/trainee.dart';
 
 class TraineeProfile extends StatefulWidget {
-  Trainee? trainee;
-  TraineeProfile(this.trainee);
+  TraineeProfile({super.key, required this.trainee});
+  Trainee trainee;
 
   @override
-  State<TraineeProfile> createState() {
-    _TraineeProfile();
-    throw UnimplementedError();
-  }
+  State<TraineeProfile> createState() => _TraineeProfile();
 }
 
 class _TraineeProfile extends State<TraineeProfile>{
-  late Trainee? trainee;
-  _TraineeProfile(){
-    trainee = widget.trainee;
-  }
+
   @override
+
   Widget build(BuildContext context) {
-    const title = "Trainee Profile";
-    MaterialApp(
-        title: title,
-        theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
-            useMaterial3: true
-        ),
-        home: Scaffold(
-          appBar: AppBar(
-              title: Text(title)
-          ),
-          resizeToAvoidBottomInset: true,
-          body: Container(
-            alignment: Alignment.center,
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    FadeInImage.assetNetwork(
-                        placeholder: "asset/media/profile_icon.gif",
-                        image: "https://firebasestorage.googleapis.com/v0/b/doh-chd-car-portal-app.appspot.com/o/Placeholders%2FprofilePicturePlaceHolder.jpg?alt=media&token=7344d9d2-156d-4fde-88eb-d1253f8b14e2"),
-                    Column(
-                      children: [
-                        // Outlined
-                      ],
-                    )
-                  ],
-                )
-              ],
-            )
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("${widget.trainee.nameFirst!} ${widget.trainee.nameMiddle!.substring(0,1)}. ${widget.trainee.nameLast!}"),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                  flex: 5,
+                  child: Image.asset("assets/media/profile_icon.gif")
+              ),
+              Expanded(
+                  flex: 15,
+                  child: Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: "First Name",
+                        ),
+                      )
+                    ],
+                  )
+              )
+            ],
           )
-        )
+        ],
+      ),
     );
-    throw UnimplementedError();
   }
 }
