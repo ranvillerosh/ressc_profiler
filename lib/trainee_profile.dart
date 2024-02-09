0dccf1fbd24daae0442fa7f37c504d49f47630b0
ranvillerosh <76862237+ranvillerosh@users.noreply.github.com>
2024-02-10 02:08:46 +0800

    main, trainee_profile

--- lib/trainee_profile.dart
+++ lib/trainee_profile.dart
@@ -4,44 +4,92 @@ import 'package:ressc_profiler/Data/trainee.dart';
 
 class TraineeProfile extends StatefulWidget {
   TraineeProfile({super.key, required this.trainee});
+
   Trainee trainee;
 
   @override
   State<TraineeProfile> createState() => _TraineeProfile();
 }
 
-class _TraineeProfile extends State<TraineeProfile>{
-
+class _TraineeProfile extends State<TraineeProfile> {
   @override
-
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("${widget.trainee.nameFirst!} ${widget.trainee.nameMiddle!.substring(0,1)}. ${widget.trainee.nameLast!}"),
-      ),
-      body: Column(
-        children: [
-          Row(
+        appBar: AppBar(
+          title: Text(
+              "${widget.trainee.nameFirst!} ${widget.trainee.nameMiddle!.substring(0, 1)}. ${widget.trainee.nameLast!}"),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          child: Column(
             children: [
-              Expanded(
-                  flex: 5,
-                  child: Image.asset("assets/media/profile_icon.gif")
-              ),
-              Expanded(
-                  flex: 15,
-                  child: Column(
-                    children: [
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: "First Name",
+              Row(
+                children: [
+                  Spacer(
+                    flex: 1,
+                  ),
+                  Expanded(
+                      flex: 8,
+                      child: FadeInImage.assetNetwork(
+                          placeholder: "asset/media/profile_icon.gif",
+                          image:
+                              "https://firebasestorage.googleapis.com/v0/b/doh-chd-car-portal-app.appspot.com/o/Placeholders%2FprofilePicturePlaceHolder.jpg?alt=media&token=7344d9d2-156d-4fde-88eb-d1253f8b14e2")),
+                  Spacer(
+                    flex: 1,
+                  ),
+                  Expanded(
+                      flex: 18,
+                      child: Container(
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                buildNameRow(
+                                    "First Name", widget.trainee.nameFirst),
+                                SizedBox(width: 10),
+                                buildNameRow(
+                                    "Middle Name", widget.trainee.nameMiddle),
+                                SizedBox(width: 10),
+                                buildNameRow(
+                                    "Last Name", widget.trainee.nameLast),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                buildOccupationRow(
+                                    "Position", widget.trainee.position),
+                                SizedBox(width: 10),
+                                buildOccupationRow(
+                                    "Office", widget.trainee.position)
+                              ],
+                            )
+                          ],
                         ),
-                      )
-                    ],
+                      )),
+                  Spacer(
+                    flex: 1,
                   )
+                ],
               )
             ],
-          )
-        ],
+          ),
+        ));
+  }
+
+  Widget buildNameRow(String label, String? initialValue) {
+    return Expanded(
+      child: TextFormField(
+        initialValue: initialValue,
+        decoration: InputDecoration(labelText: label),
+      ),
+    );
+  }
+
+  Widget buildOccupationRow(String label, String? initialValue) {
+    return Expanded(
+      child: TextFormField(
+        initialValue: initialValue,
+        decoration: InputDecoration(labelText: label),
       ),
     );
   }
