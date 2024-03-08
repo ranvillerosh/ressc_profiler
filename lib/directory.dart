e55d57afe7767ef298179d9f4dc4fa7728e0c5bd
ranvillerosh <ranvillerosh@gmail.com>
2024-03-08 18:04:04 +0800

    search

--- lib/directory.dart
+++ lib/directory.dart
@@ -88,6 +88,52 @@ class _RESSCDirectory extends State<RESSCDirectory> with TickerProviderStateMixi
     );
   }
 
+  Widget buildSearch(BuildContext context) {
+    return SearchAnchor(
+        builder: (BuildContext context, SearchController controller) {
+          return SearchBar(
+            controller: controller,
+            padding: const MaterialStatePropertyAll<EdgeInsets>(
+                EdgeInsets.symmetric(horizontal: 16.0)),
+            onTap: () {
+              controller.openView();
+            },
+            onChanged: (_) {
+              controller.openView();
+            },
+            leading: const Icon(Icons.search),
+            trailing: <Widget>[
+              Tooltip(
+                message: 'Change brightness mode',
+                child: IconButton(
+                  isSelected: isDark,
+                  onPressed: () {
+                    setState(() {
+                      isDark = !isDark;
+                    });
+                  },
+                  icon: const Icon(Icons.wb_sunny_outlined),
+                  selectedIcon: const Icon(Icons.brightness_2_outlined),
+                ),
+              )
+            ],
+          );
+        }, suggestionsBuilder:
+        (BuildContext context, SearchController controller) {
+      return List<ListTile>.generate(5, (int index) {
+        final String item = 'item $index';
+        return ListTile(
+          title: Text(item),
+          onTap: () {
+            setState(() {
+              controller.closeView(item);
+            });
+          },
+        );
+      });
+    });
+  }
+
   Widget directoryStream() {
     return StreamBuilder<QuerySnapshot>(
         stream: GlobalData.db.collection('trainee').snapshots(),
